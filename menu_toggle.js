c6bd05226a98fbe52873f1caa4408145769a4b79
Marco Grochowiak <marco.grochowiak@outlook.com>
2024-09-25 09:18:02 +0200

    update fokus auf Desktop Darstellung

--- menu_toggle.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const toggleButton = document.querySelector('.menu-toggle');
-    const header = document.querySelector('header');
-
-    toggleButton.addEventListener('click', function() {
-        header.classList.toggle('menu-open'); // Fügt eine Klasse hinzu, die das Menü öffnet oder schließt
-    });
-});
